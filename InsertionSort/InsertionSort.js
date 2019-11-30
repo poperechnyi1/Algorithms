e5530d2be7df0a98e5dcb99849ddc0c896b3915a
spoperechnyi <poperechnyi1@gmail.com>
2019-11-30 22:12:08 +0200

    improve code

--- InsertionSort/InsertionSort.js
+++ InsertionSort/InsertionSort.js
@@ -1,19 +1,19 @@
 //Simple insertion sort
 
-let arrayForSorting = [58, 31,41,59,26,41,0];
+let arrayForSorting = [58, 31, 41, 59, 26, 41, 0, -1];
 
 // sort in rising order
-for(let i = 1; i < arrayForSorting.length; i++)
-{
-    let key = arrayForSorting[i];
-
-    while(i>0 && arrayForSorting[i - 1] >key)
-    {
-        j = i - 1;
-        arrayForSorting[i] = arrayForSorting[j];
-        arrayForSorting[j] = key;
-        i = j;
-    }
+for (let j = 0; j < arrayForSorting.length; j++) {
+  let key = arrayForSorting[j];
+  let i = j - 1;
+  while(i>=0 && arrayForSorting[i] > key)
+  {
+    arrayForSorting[i+1] = arrayForSorting[i];
+    i = i-1;
+  }
+  arrayForSorting[i+1] = key;
 }
 
-console.log("There is a sorting in rising order: ",arrayForSorting);
+console.log("There is a sorting in rising order: ", arrayForSorting);
+
+//sort in descending order
