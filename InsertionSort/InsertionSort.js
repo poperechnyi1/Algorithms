f7fb452be1326585a1b84e675653fc6132da571d
spoperechnyi <poperechnyi1@gmail.com>
2019-12-01 17:10:56 +0200

    improve algorithms and add reverse sorting

--- InsertionSort/InsertionSort.js
+++ InsertionSort/InsertionSort.js
@@ -1,19 +1,32 @@
 //Simple insertion sort
 
-let arrayForSorting = [58, 31, 41, 59, 26, 41, 0, -1];
+let arrayForSorting = [41, 58, 31, 59, 26, 41, 0, -1];
 
-// sort in rising order
-for (let j = 0; j < arrayForSorting.length; j++) {
-  let key = arrayForSorting[j];
-  let i = j - 1;
-  while(i>=0 && arrayForSorting[i] > key)
-  {
-    arrayForSorting[i+1] = arrayForSorting[i];
-    i = i-1;
+function SortByRisingOrder() {
+  for (let j = 0; j < arrayForSorting.length; j++) {
+    let key = arrayForSorting[j];
+    let i = j - 1;
+    while (i >= 0 && arrayForSorting[i] > key) {
+      arrayForSorting[i + 1] = arrayForSorting[i];
+      i = i - 1;
+    }
+    arrayForSorting[i + 1] = key;
   }
-  arrayForSorting[i+1] = key;
+  console.log("There is a sorted array in rising order: ", arrayForSorting);
 }
 
-console.log("There is a sorting in rising order: ", arrayForSorting);
+function SortByDescendingOrder() {
+  for (let j = 0; j < arrayForSorting.length; j++) {
+    let key = arrayForSorting[j];
 
-//sort in descending order
+    for (var i = j - 1; i >= 0 && key > arrayForSorting[i]; i--) {
+      arrayForSorting[i + 1] = arrayForSorting[i];
+      arrayForSorting[i] = key;
+    }
+  }
+
+  console.log("There is a sorted array in descending order: ", arrayForSorting);
+}
+
+SortByDescendingOrder();
+SortByRisingOrder();
