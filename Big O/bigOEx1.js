6d0056439f076f3dac007c2bcf72c8a4d3e72aa0
Stepan Poperechnyi <stepan.poperechnyi@connectwise.com>
2021-05-26 12:52:06 +0300

    exact calculation

--- Big O/bigOEx1.js
+++ Big O/bigOEx1.js
@@ -2,17 +2,17 @@
 let inputArr = new Array(1000).fill('Kote')
 
 function funChallenge(input) {
-  let a = 10;
-  a = 50 + 3;
+  let a = 10; //O(1)
+  a = 50 + 3;//O(1)
 
-  for (let i = 0; i < input.length; i++) {
-    console.log(a)
-    let stranger = true;
-    a++;
+  for (let i = 0; i < input.length; i++) { //O(n)
+    console.log(a)//O(n)
+    let stranger = true;//O(n)
+    a++;//O(n)
   }
-  return a;
+  return a;//O(1)
 }
 
 funChallenge(inputArr)
 
-// it is O(n) linear
+// it is O(4n + 3) linear
