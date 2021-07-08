8c4d1b254c302c2a41ec536fbfdd449b033f7675
Stepan Poperechnyi <poperechnyi1@gmail.com>
2021-07-08 21:21:15 +0300

    Merge sorted arrays

--- Arrays/mergeSortedArray.js
+++ Arrays/mergeSortedArray.js
@@ -1,49 +1,92 @@
 function mergeSortedArrays(arr1, arr2) {
-  let maxLength = 0
-  let mergedArray = []
-  if (arr1.length > 0 && arr1.length > arr2.length) {
-    maxLength = arr1.length
-    mergedArray = merge(maxLength, arr1, arr2)
-  } else if (arr2.length > 0 && arr2.length > arr1.length) {
-    maxLength = arr2.length
-    mergedArray = merge(maxLength, arr2, arr1)
-  } else if (arr2.length === arr1.length) {
-    maxLength = arr2.length
-    mergedArray = merge(maxLength, arr2, arr1)
-  } else if (arr1.length === 0 && arr2.length === 0){
-    return 'Ooops arrays are empty'
+  let maxLength = arr1.length + arr2.length;
+  let mergedArray = [];
+  if (arr1.length === 0 && arr2.length === 0) {
+    return "Ooops arrays are empty";
   }
 
-  return mergedArray
+  mergedArray = merge(maxLength, arr1, arr2);
+
+  if (mergedArray.length !== maxLength) {
+    return "Ooop something wrong with realization fo algorithm";
+  }
+  return mergedArray;
 }
 
 function merge(iterationCount, arr1, arr2) {
-  let mergedArray = []
-  for (let i = 0; i < iterationCount; i++)
-  {
-    if (i < arr1.length && i < arr2.length) {
-      if (arr1[i] > arr2[i]) {
-        mergedArray.push(arr2[i])
-        mergedArray.push(arr1[i])
-      } else if (arr1[i] < arr2[i]) {
-        mergedArray.push(arr1[i])
-        mergedArray.push(arr2[i])
-      } else {
-        mergedArray.push(arr1[i])
-        mergedArray.push(arr2[i])
+  let mergedArray = [];
+  let iterationCount1 = 0;
+  let iterationCount2 = 0;
+  console.log(iterationCount);
+  for (let i = 0; i < iterationCount; i++) {
+    if (
+      (iterationCount1 === 0 && iterationCount2 === 0) ||
+      iterationCount1 === iterationCount2
+    ) {
+      if (arr1[iterationCount1] === arr2[iterationCount2]) {
+        mergedArray.push(arr1[iterationCount1]);
+        mergedArray.push(arr2[iterationCount2]);
+        iterationCount1++;
+        iterationCount2++;
+        continue;
       }
-    }
 
-    if (i < arr1.length && i >= arr2.length) {
-      mergedArray.push(arr1[i])
-    } 
+      if (arr1[iterationCount1] > arr2[iterationCount2]) {
+        console.log(35);
+        mergedArray.push(arr2[iterationCount2]);
+        iterationCount2++;
+        continue;
+      }
 
-    if (i >= arr1.length && i < arr2.length) {
-      mergedArray.push(arr2[i])
+      if (arr1[iterationCount1] < arr2[iterationCount2]) {
+        console.log(41);
+        mergedArray.push(arr1[iterationCount1]);
+        iterationCount1++;
+        continue;
+      }
+    } else {
+      if (arr1[iterationCount1] === arr2[iterationCount2]) {
+        mergedArray.push(arr1[iterationCount1]);
+        mergedArray.push(arr2[iterationCount2]);
+        iterationCount1++;
+        iterationCount2++;
+        continue;
+      }
+
+      if (arr1[iterationCount1] > arr2[iterationCount2]) {
+        mergedArray.push(arr2[iterationCount2]);
+        iterationCount2++;
+        continue;
+      }
+
+      if (arr1[iterationCount1] < arr2[iterationCount2]) {
+        mergedArray.push(arr1[iterationCount1]);
+        iterationCount1++;
+        continue;
+      }
     }
   }
 
-  return mergedArray
+  console.log(mergedArray);
+
+  if (arr1.length - 1 > iterationCount1) {
+    for (let j = iterationCount1; j < arr1.length; j++) {
+      mergedArray.push(arr1[j]);
+    }
+  }
+
+  if (arr2.length - 1 > iterationCount2) {
+    for (let k = iterationCount2; k < arr2.length; k++) {
+      mergedArray.push(arr2[k]);
+    }
+  }
+
+  return mergedArray;
 }
 
-console.log(mergeSortedArrays([0, 3, 4, 5, 31, 32], [3, 4, 6, 7, 30, 35]));
+console.log(
+  mergeSortedArrays(
+    [-7, -6, -5, -4, -3, -2, -1, 0, 8],
+    [-105, 23, 88, 100, 110, 130, 140, 150, 160, 170]
+  )
+);
