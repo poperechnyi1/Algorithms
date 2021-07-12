db77419d4e6c636c176c7f7bc1b4715d7f8e5de0
Stepan Poperechnyi <poperechnyi1@gmail.com>
2021-07-12 21:58:44 +0300

    correct logic

--- Arrays/twoSum.js
+++ Arrays/twoSum.js
@@ -5,19 +5,22 @@ var twoSum = function (nums, target) {
   }
   let startMarker = 0;
   let secondMarker = 1;
-  while (sum != target) {
+  while (nums[startMarker] + nums[secondMarker] != target) {
     sum = nums[startMarker] + nums[secondMarker];
+    console.log(10, sum);
     if (sum != target) {
-      if (secondMarker != nums.length - 1) {
-        secondMarker++;
-      } else {
+      if (secondMarker === nums.length - 1) {
         startMarker++;
         secondMarker = startMarker + 1;
+      } else {
+        secondMarker++;
       }
     }
   }
 
+  //   console.log(startMarker, secondMarker);
+
   return [startMarker, secondMarker];
 };
 
-console.log(twoSum([3, 3], 6));
+console.log(twoSum([0, 4, 3, 0], 0));
